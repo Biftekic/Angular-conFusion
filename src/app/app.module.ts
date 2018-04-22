fac14ee480b0118bd988a7e19bf7b7501254e5f2
Tin Pavlic <tinpavlic@gmail.com>
2018-04-22 22:12:40 +0200

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    FlexLayoutModule
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule
   ],
    providers: [],
   bootstrap: [AppComponent]
